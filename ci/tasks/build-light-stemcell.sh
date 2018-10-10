534c5d7398209ed6d235bb36a11971942821d254
Max Petersen <mpetersen@pivotal.io>
2018-10-10 16:19:02 -0700

    Authenticate to S3 when checking for previously uploaded stemcells
    [#160973608](https://www.pivotaltracker.com/story/show/160973608)
    
    Co-authored-by: Luan Santos <lsantos@pivotal.io>

--- ci/tasks/build-light-stemcell.sh
+++ ci/tasks/build-light-stemcell.sh
@@ -3,7 +3,26 @@
 set -eu
 
 : ${BUCKET_NAME:?}
-: ${BOSH_IO_BUCKET_NAME:?} # used to check if current stemcell already exists
+: ${STEMCELL_BUCKET_NAME:?} # used to check if current stemcell already exists
+: ${AWS_ACCESS_KEY_ID:?}
+: ${AWS_SECRET_ACCESS_KEY:?}
+
+stemcell_url() {
+  resource="/${STEMCELL_BUCKET_NAME}/${light_stemcell_name}"
+
+  if [ ! -z "$AWS_ACCESS_KEY_ID" ]; then
+    expires=$(date +%s)
+    expires=$((expires + 30))
+
+    string_to_sign="HEAD\n\n\n${expires}\n${resource}"
+    signature=$(echo -en "$string_to_sign" | openssl sha1 -hmac ${AWS_SECRET_ACCESS_KEY} -binary | base64)
+    signature=$(python -c "import urllib; print urllib.quote_plus('${signature}')")
+
+    echo -n "https://s3.amazonaws.com${resource}?AWSAccessKeyId=${AWS_ACCESS_KEY_ID}&Expires=${expires}&Signature=${signature}"
+  else
+    echo -n "https://s3.amazonaws.com${resource}"
+  fi
+}
 
 # inputs
 builder_src="$PWD/builder-src"
@@ -23,12 +42,12 @@ light_stemcell_name="light-${original_stemcell_name}"
 
 echo "Using raw stemcell name: $raw_stemcell_name"
 
-bosh_io_light_stemcell_url="https://s3.amazonaws.com/$BOSH_IO_BUCKET_NAME/$light_stemcell_name"
+light_stemcell_url="$(stemcell_url)"
 set +e
-wget --spider "$bosh_io_light_stemcell_url"
+wget --spider "$light_stemcell_url"
 if [[ "$?" == "0" ]]; then
   echo "Google light stemcell '$light_stemcell_name' already exists!"
-  echo "You can download here: $bosh_io_light_stemcell_url"
+  echo "You can download here: $light_stemcell_url"
   exit 1
 fi
 set -e
